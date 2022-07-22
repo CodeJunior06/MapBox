248bb8ac30737cb2e2439db5bc683dc25f18d134
CodeJunior06 <san1428.srp@gmail.com>
2022-07-22 08:17:58 -0300

    Add sdk mapbox

--- app/src/main/java/com/codejunior/mapbox/MainActivity.kt
+++ app/src/main/java/com/codejunior/mapbox/MainActivity.kt
@@ -2,10 +2,44 @@ package com.codejunior.mapbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codejunior.mapbox.databinding.ActivityMainBinding
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.maps.Style
 
 class MainActivity : AppCompatActivity() {
+
+    private var mapView: com.mapbox.maps.MapView? = null
+    private lateinit var _binding: ActivityMainBinding
+    private val binding get() = _binding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+   /*     mapView = binding.mapView
+        mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)*/
+
+
+    }
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
     }
 }
